0a39dbdfa12a4a03dbf9b3912baa8b337132e250
johninamillion <johninamio@gmail.com>
2022-01-19 20:12:44 +0100

    add getTemplatePart

--- src/View.php
+++ src/View.php
@@ -4,6 +4,31 @@ namespace CMS;
 
 class View {
 
+    /**
+     * Get template part
+     * @access  public
+     * @param   string  $template_part
+     * @return  void
+     */
+    public function getTemplatePart( string $template_part ) : void {
+        /** @var string $template_file */
+        $template_file = APPLICATION_TEMPLATE_DIR . DIRECTORY_SEPARATOR . "$template_part.php";
 
+        // Überprüfen ob das Template existiert und eine Fehlermeldung ausgeben, falls nicht
+        if ( file( $template_file ) !== FALSE ) {
+            trigger_error(
+                sprintf(
+                    'Template part not found (%1$s).',
+                    $template_file
+                ),
+                E_USER_NOTICE
+            );
+        }
+        // Template einbinden, wenn dieses existiert
+        else {
+
+            include $template_file;
+        }
+    }
 
 }
