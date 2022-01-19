c4543df2e02d14253a18330c5188e3c6a4f9f154
johninamillion <johninamio@gmail.com>
2022-01-19 20:41:03 +0100

    add database credentials

--- config.php
+++ config.php
@@ -2,5 +2,13 @@
 
 namespace CMS;
 
+// Anwendungsverzeichnisse
 define( 'APPLICATION_ROOT_DIR',     __DIR__ );
 define( 'APPLICATION_TEMPLATE_DIR', __DIR__ . DIRECTORY_SEPARATOR . 'templates' );
+
+// Datenbankkonfiguration
+define( 'DB_HOST',      'localhost' );
+define( 'DB_NAME',      'sae_wbd0921_5100_cms' );
+define( 'DB_USER',      'root' );
+define( 'DB_PASS',      'root' );
+define( 'DB_PORT',      '3306' );
