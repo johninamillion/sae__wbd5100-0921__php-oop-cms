10887c18a6018e3292318274d03b775caed6a6fa
johninamillion <johninamio@gmail.com>
2022-01-19 19:42:49 +0100

    create Controller class

---
+++ src/Controller.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace CMS;
+
+abstract class Controller {
+
+    /**
+     * Controller index method
+     * @access  public
+     * @return  void
+     */
+    abstract public function index() : void;
+
+}
