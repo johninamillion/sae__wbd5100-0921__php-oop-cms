0792fe8d0cec643bf767d17db28af1f8db982314
johninamillion <johninamio@gmail.com>
2022-01-19 20:30:19 +0100

    create login and register templates

---
+++ templates/login/index.php
@@ -0,0 +1,15 @@
+
+<form id="login-form" class="form" method="post">
+    <h1><?= _( 'Login' ) ?></h1>
+    <div class="form__row">
+        <label class="form__label" for="username"><?= _( 'Username' ) ?></label>
+        <input class="form__input form__input--text" id="username" name="username" type="text">
+    </div>
+    <div class="form__row">
+        <label class="form__label" for="password"><?= _( 'Password' ) ?></label>
+        <input class="form__input form__input--text" id="password" name="password" type="password">
+    </div>
+    <div class="form__row">
+        <input class="form__input form__input--submit" id="submit" value="<?= _( 'Login' ) ?>">
+    </div>
+</form>
---
+++ templates/register/index.php
@@ -0,0 +1,23 @@
+
+<form id="register-form" class="form" method="post">
+    <h1><?= _( 'Register' ) ?></h1>
+    <div class="form__row">
+        <label class="form__label" for="username"><?= _( 'Username' ) ?></label>
+        <input class="form__input form__input--text" id="username" name="username" type="text">
+    </div>
+    <div class="form__row">
+        <label class="form__label" for="email"><?= _( 'E-Mail' ) ?></label>
+        <input class="form__input form__input--text" id="email" name="email" type="email">
+    </div>
+    <div class="form__row">
+        <label class="form__label" for="password"><?= _( 'Password' ) ?></label>
+        <input class="form__input form__input--text" id="password" name="password" type="password">
+    </div>
+    <div class="form__row">
+        <label class="form__label" for="password_repeat"><?= _( 'Repeat Password' ) ?></label>
+        <input class="form__input form__input--text" id="password_repeat" name="password_repeat" type="password">
+    </div>
+    <div class="form__row">
+        <input class="form__input form__input--submit" id="submit" value="<?= _( 'Register' ) ?>">
+    </div>
+</form>
