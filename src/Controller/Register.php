cb012916fe50a0e10bb791bae2f42543de2abeca
johninamillion <johninamio@gmail.com>
2022-01-19 21:01:09 +0100

    call register on user model

--- src/Controller/Register.php
+++ src/Controller/Register.php
@@ -3,8 +3,21 @@
 namespace CMS\Controller;
 
 use CMS\Controller;
+use CMS\Model\User as UserModel;
 
 final class Register extends Controller {
+
+    private ?User $User = NULL;
+
+    /**
+     * @access  public
+     * @constructor
+     */
+    public function __construct() {
+        $this->User = new UserModel();
+
+        parent::__construct();
+    }
 
     /**
      * Controller index method
@@ -12,6 +25,10 @@ final class Register extends Controller {
      * @return  void
      */
     public function index(): void {
+        if ( $this->isMethod( self::METHOD_POST ) ) {
+            $this->User->register();
+        }
+
         $this->View->getTemplatePart( 'header' );
         $this->View->getTemplatePart( 'register/index' );
         $this->View->getTemplatePart( 'footer' );
