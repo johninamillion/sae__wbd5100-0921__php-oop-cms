79eaa74d08ecd9e1b63e3150fbfa4eb81a98a3f0
johninamillion <johninamio@gmail.com>
2022-01-19 19:44:48 +0100

    create Index controller

---
+++ src/Controller/Index.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace CMS\Controller;
+
+use CMS\Controller;
+
+final class Index extends Controller {
+
+    /**
+     * Controller index method
+     * @access  public
+     * @return  void
+     */
+    public function index(): void {
+        echo "Hello World!";
+    }
+
+}
