af2f0f7413e5d4029012fee0b1f3ce41730670b7
johninamillion <johninamio@gmail.com>
2022-01-19 20:47:59 +0100

    create User model

---
+++ src/Model/User.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace CMS\Model;
+
+use CMS\Model;
+
+final class User extends Model {
+
+    /**
+     * @access  public
+     * @return  bool
+     */
+    public function logout() : bool {
+        // TODO: create user logout
+
+        return FALSE;
+    }
+
+    /**
+     * @access  public
+     * @return  bool
+     */
+    public function login() : bool {
+        // TODO: create user login
+
+        return FALSE;
+    }
+
+    /**
+     * @access  public
+     * @return  bool
+     */
+    public function register() : bool {
+        // TODO: create user registration
+
+        return FALSE;
+    }
+
+}
