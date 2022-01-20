b94a05ddb53393bc6a52600176bc2924a626c114
johninamillion <johninamio@gmail.com>
2022-01-20 15:55:40 +0100

    redirect user to index if login was successfull

--- src/Controller/Login.php
+++ src/Controller/Login.php
@@ -3,8 +3,21 @@
 namespace CMS\Controller;
 
 use CMS\Controller;
+use CMS\Model\User as UserModel;
 
 final class Login extends Controller {
+
+    private ?UserModel $User = NULL;
+
+    /**
+     * @access  public
+     * @constructor
+     */
+    public function __construct() {
+        $this->User = new UserModel();
+
+        parent::__construct();
+    }
 
     /**
      * Controller index method
@@ -12,6 +25,10 @@ final class Login extends Controller {
      * @return  void
      */
     public function index(): void {
+        if ( $this->isMethod( self::METHOD_POST ) && $this->User->login() ) {
+            $this->redirect( '/' );
+        }
+
         $this->View->getTemplatePart( 'header' );
         $this->View->getTemplatePart( 'login/index' );
         $this->View->getTemplatePart( 'footer' );
