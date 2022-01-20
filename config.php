34ea74cdbe9d86818c8b514a657dc93371264e95
johninamillion <johninamio@gmail.com>
2022-01-20 20:00:05 +0100

    add define for debugging

--- config.php
+++ config.php
@@ -2,6 +2,9 @@
 
 namespace CMS;
 
+// Debugging
+define( 'DEBUG',        TRUE );
+
 // Anwendungsverzeichnisse
 define( 'APPLICATION_ROOT_DIR',     __DIR__ );
 define( 'APPLICATION_TEMPLATE_DIR', __DIR__ . DIRECTORY_SEPARATOR . 'templates' );
