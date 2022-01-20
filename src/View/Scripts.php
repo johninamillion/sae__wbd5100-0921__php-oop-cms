7dceb2b497cb3b5af58e7dbfc7fad2b9a6654d83
johninamillion <johninamio@gmail.com>
2022-01-20 19:07:21 +0100

    create Scripts class

---
+++ src/View/Scripts.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace CMS\View;
+
+final class Scripts {
+
+    private array $scripts = [];
+
+    /**
+     * Add script
+     * @access  public
+     * @param   string  $id
+     * @param   string  $src
+     * @return  void
+     */
+    public function addScript( string $id, string $src ) : void {
+        $this->scripts[ $id ] = $src;
+    }
+
+    /**
+     * Print scripts
+     * @access  public
+     * @return  void
+     */
+    public function printScripts() : void {
+        foreach ( $this->scripts as $id => $src ) {
+            echo "<script id=\"{$id}\" type=\"text/javascript\" src=\"{$src}\"></script>";
+        }
+    }
+
+}
