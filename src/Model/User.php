5bc862addc879f1d065b89eecb2c81c848f4dad9
johninamillion <johninamio@gmail.com>
2022-01-20 15:24:48 +0100

    complete user input validation

--- src/Model/User.php
+++ src/Model/User.php
@@ -2,6 +2,8 @@
 
 namespace CMS\Model;
 
+use CMS\Controller\Error;
+use CMS\Errors;
 use CMS\Model;
 
 final class User extends Model {
@@ -90,9 +92,20 @@ final class User extends Model {
      * @return  bool
      */
     private function validateEmail( ?string $email ) : bool {
-        // TODO: validate email
+        // Überprüfen ob eine E-Mail Adresse eingegeben wurde
+        if ( is_null( $email ) === TRUE ) {
+            Errors::addError( 'email', _( 'Please type in a valid E-Mail address' ) );
+        }
+        // Überprüfen ob die E-Mail Adresse bereits für einen Nutzer existiert
+        if ( $this->emailExists( $email ) === TRUE ) {
+            Errors::addError( 'email', _( 'E-Mail address already exists' ) );
+        }
+        // Überprüfen ob die E-Mail Adresse valide ist
+        if ( filter_var( $email, FILTER_VALIDATE_EMAIL ) === FALSE ) {
+            Errors::addError( 'email', _( 'E-Mail address should be valid' ) );
+        }
 
-        return TRUE;
+        return Errors::hasErrors( 'email' ) === FALSE;
     }
 
     /**
@@ -103,9 +116,44 @@ final class User extends Model {
      * @return  bool
      */
     private function validatePassword( ?string $password, ?string $password_repeat ) : bool {
-        // TODO: validate password
+        // Überprüfen ob ein Passwort eingegeben wurde
+        if ( is_null( $password ) === TRUE ) {
+            Errors::addError( 'password', _( 'Please type in a valid password' ) );
+        }
+        // Überprüfen ob ein Passwort wiederholteingegeben wurde
+        if ( is_null( $password_repeat ) === TRUE ) {
+            Errors::addError( 'password_repeat', _( 'Please repeat a valid password' ) );
+        }
+        // Überprüfen ob das Passwort mindestens 8 Zeichen lang ist
+        if ( strlen( $password ) < 8 ) {
+            Errors::addError( 'password', _( 'Password should be minimum 8 characters long' ) );
+        }
+        // Überprüfen ob die Eingabe der Wiederholung dem eingegebenen Passwort entspricht
+        if ( $password !== $password_repeat ) {
+            Errors::addError( 'password_repeat', _( 'Please repeat a the password' ) );
+        }
+        // Überprüfen ob das Passwort leerzeichen enthält
+        if ( preg_match( '/\s/', $password ) == TRUE ) {
+            Errors::addError( 'password', _( 'Password should not contain any whitespace' ) );
+        }
+        // Überprüfen ob das Passwort Kleinbuchstaben enthält
+        if ( preg_match( '/[a-z]/', $password ) == FALSE ) {
+            Errors::addError( 'password', _( 'Password should contain minimum one small letter' ) );
+        }
+        // Überprüfen ob das Passwort Großbuchstaben enthält
+        if ( preg_match( '/[A-Z]/', $password ) == FALSE ) {
+            Errors::addError( 'password', _( 'Password should contain minimum one capital letter' ) );
+        }
+        // Überprüfen ob das Passwort Zahlen enthält
+        if ( preg_match( '/\d/', $password ) == FALSE ) {
+            Errors::addError( 'password', _( 'Password should contain minimum one digit' ) );
+        }
+        // Überprüfen ob das Passwort Sonderzeichen enthält
+        if ( preg_match( '/\W/', $password ) == FALSE ) {
+            Errors::addError( 'password', _( 'Password should contain minimum one special character' ) );
+        }
 
-        return TRUE;
+        return Errors::hasErrors( 'password' ) === FALSE && Errors::hasErrors( 'password_repeat' ) === FALSE;
     }
 
     /**
@@ -115,9 +163,24 @@ final class User extends Model {
      * @return  bool
      */
     private function validateUsername( ?string $username ) : bool {
-        // TODO: validate username
+        // Überprüfen ob ein Nutzername eingegeben wurde
+        if ( is_null( $username ) === TRUE ) {
+            Errors::addError( 'username', _( 'Please type in a valid username' ) );
+        }
+        // Überprüfen ob der Nutzername bereits für einen Nutzer existiert
+        if ( $this->usernameExists( $username ) === TRUE ) {
+            Errors::addError( 'username', _( 'Username already exists' ) );
+        }
+        // Überprüfen ob der Nutzername mindestens 4 Zeichen enthält
+        if ( strlen( $username ) < 4 ) {
+            Errors::addError( 'username', _( 'Username should be minimum 4 characters long' ) );
+        }
+        // Überprüfen ob der Nutzername maximal 16 Zeichen enthält
+        if ( strlen( $username ) > 16 ) {
+            Errors::addError( 'username', _( 'Username should be maxmimum 16 characters long' ) );
+        }
 
-        return TRUE;
+        return Errors::hasErrors( 'username' ) === FALSE;
     }
 
     /**
@@ -179,7 +242,7 @@ final class User extends Model {
             $Statement->bindValue( ':email', $email );
             $Statement->bindValue( ':password', $hashed_password );
             $Statement->bindValue( ':salt', $salt );
-            $Statement->bindValue( 'registered', $registered );
+            $Statement->bindValue( ':registered', $registered );
             $Statement->execute();
 
             return $Statement->rowCount() > 0;
