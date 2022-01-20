04e79213e068057db4417c3a8c1b6fcb1f96f3a5
johninamillion <johninamio@gmail.com>
2022-01-20 16:08:49 +0100

    add method to authorize user login

--- src/Controller/User.php
+++ src/Controller/User.php
@@ -15,4 +15,23 @@ final class User extends Controller {
         echo "Hello User!";
     }
 
+    /**
+     * Controller settings method
+     * @access  public
+     * @return  void
+     */
+    public function settings() : void {
+        echo "User Settings";
+    }
+
+    /**
+     * Controller profile method
+     * @access  public
+     * @param   string|NULL $username
+     * @return  void
+     */
+    public function profile( ?string $username = NULL ) : void {
+        echo "User Profile";
+    }
+
 }
