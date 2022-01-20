131ab964a5714e1977823d4dc6e8d70015297710
johninamillion <johninamio@gmail.com>
2022-01-20 15:34:44 +0100

    print input errors

--- templates/login/index.php
+++ templates/login/index.php
@@ -1,9 +1,15 @@
+<?php
+
+namespace CMS;
+
+?>
 
 <form id="login-form" class="form" method="post">
     <h1><?= _( 'Login' ) ?></h1>
     <div class="form__row">
         <label class="form__label" for="username"><?= _( 'Username' ) ?></label>
         <input class="form__input form__input--text" id="username" name="username" type="text">
+        <?php Errors::printInputErrors( 'username' ); ?>
     </div>
     <div class="form__row">
         <label class="form__label" for="password"><?= _( 'Password' ) ?></label>
