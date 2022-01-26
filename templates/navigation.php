1b6c7757a6e107a2d3465a3bf5f6560a586db4a6
johninamillion <johninamio@gmail.com>
2022-01-26 10:46:12 +0100

    add anchor for posts

--- templates/navigation.php
+++ templates/navigation.php
@@ -2,6 +2,7 @@
 <header id="masthead">
     <nav id="masthead__navigation" class="navigation">
         <ul class="navigation__list">
+            <li class="navigation__list-item"><a class="navigation__anchor" href="/posts"><?= _( 'Posts' ) ?></a></li>
             <li class="navigation__list-item"><a class="navigation__anchor" href="/user/settings"><?= _( 'Settings' ) ?></a></li>
             <li class="navigation__list-item"><a class="navigation__anchor" href="/user/profile"><?= _( 'Profile' ) ?></a></li>
             <li class="navigation__list-item"><a class="navigation__anchor" href="/logout"><?= _( 'Logout' ) ?></a></li>
