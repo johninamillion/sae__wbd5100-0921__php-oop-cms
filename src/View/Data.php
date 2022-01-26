3347cbbf9241853f346e90ca91bab49d64b564a2
johninamillion <johninamio@gmail.com>
2022-01-26 11:22:45 +0100

    add methods to format date and time

--- src/View/Data.php
+++ src/View/Data.php
@@ -28,4 +28,37 @@ final class Data {
         $this->data[ $key ] = $value;
     }
 
+    /**
+     * Format date from timestamp
+     * @access  public
+     * @param   int     $timestamp
+     * @return  string
+     */
+    public function formatDate( int $timestamp ) : string {
+
+        return date( DATE_FORMAT, $timestamp );
+    }
+
+    /**
+     * Format date and time from timestamp
+     * @access  public
+     * @param   int     $timestamp
+     * @return  string
+     */
+    public function formatDateTime( int $timestamp ) : string {
+
+        return date( DATE_FORMAT . DATE_TIME_SEPARATOR . TIME_FORMAT, $timestamp );
+    }
+
+    /**
+     * Format time from timestamp
+     * @access  public
+     * @param   int     $timestamp
+     * @return  string
+     */
+    public function formatTime( int $timestamp ) : string {
+
+        return date( TIME_FORMAT, $timestamp );
+    }
+
 }
