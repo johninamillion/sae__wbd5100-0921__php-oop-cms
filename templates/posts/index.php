614ab991db882c12922f50da1149b01c34c27ec9
johninamillion <johninamio@gmail.com>
2022-01-26 11:23:46 +0100

    render posts

--- templates/posts/index.php
+++ templates/posts/index.php
@@ -24,3 +24,18 @@ namespace CMS;
     </div>
 
 </form>
+
+<section id="posts" class="section">
+    <?php foreach ( $this->Data->posts as $post ): ?>
+        <article class="article post">
+            <div class="article__body post__body">
+                <h1 class="article__heading post__heading"><?= $post[ 'post_title' ] ?></h1>
+                <div>
+                    <?= _( 'Author:' ) ?> <a href="/user/profile/<?= $post[ 'user_username' ] ?>"><?= $post[ 'user_username' ] ?></a>
+                    <?= _( 'Created:' ) ?> <span><?= $this->Data->formatDateTime( $post[ 'post_created' ] ) ?></span>
+                </div>
+                <p><?= $post[ 'post_message' ] ?></p>
+            </div>
+        </article>
+    <?php endforeach; ?>
+</section>
