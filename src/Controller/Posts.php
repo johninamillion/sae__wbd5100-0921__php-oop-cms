81f4d44b05750499040e332457be93ba3daa6d03
johninamillion <johninamio@gmail.com>
2022-01-26 11:44:57 +0100

    create method for view of a single post

--- src/Controller/Posts.php
+++ src/Controller/Posts.php
@@ -37,13 +37,40 @@ final class Posts extends Controller {
             }
         }
 
-        $this->View->Data->posts = $this->Posts->getPosts();
+        /** @var array $posts */
+        $posts = $this->Posts->getPosts();
+
+        $this->View->Data->posts = $posts;
 
         $this->View->Document->setTitle( _( 'Posts' ) );
 
         $this->View->getTemplatePart( 'header' );
         $this->View->getTemplatePart( 'navigation' );
         $this->View->getTemplatePart( 'posts/index' );
+        $this->View->getTemplatePart( 'footer' );
+    }
+
+    /**
+     * View single post
+     * @access  public
+     * @param   string|NULL $post_id
+     * @return  void
+     */
+    public function view( ?string $post_id = NULL ) : void {
+        // Überprüfen ob die post id existiert, und einen Error 404 ausspielen, wenn nicht
+        if ( $this->Posts->postIdExists( $post_id ) === FALSE ) {
+            Error::init();
+        }
+
+        /** @var array $post */
+        $post = $this->Posts->getPost( $post_id );
+
+        $this->View->Data->post = $post;
+
+        $this->View->Document->setTitle( $post[ 'post_title' ] );
+
+        $this->View->getTemplatePart( 'header' );
+        $this->View->getTemplatePart( 'navigation' );
         $this->View->getTemplatePart( 'footer' );
     }
 
