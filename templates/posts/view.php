eaffff9e6d7752e098ff3c3d48735725d78c23c5
johninamillion <johninamio@gmail.com>
2022-01-27 11:41:09 +0100

    add comment form

--- templates/posts/view.php
+++ templates/posts/view.php
@@ -19,4 +19,24 @@ namespace CMS;
         </article>
     </section>
 
+    <section id="comments" class="section">
+        <form id="comment-form" class="form box" method="post">
+            <div class="form__header">
+                <h2>Comment the Post</h2>
+                <?php Messages::printFormSuccess( 'create_comment' ); ?>
+            </div>
+            <div class="form__row">
+                <label class="form__label" for="comment">Comment</label>
+                <div class="form__input-wrapper">
+                    <input class="form__input form__input--hidden" name="post_id" type="hidden" value="<?= $this->Data->post[ 'post_id' ] ?>">
+                    <textarea class="form__input form__input--textarea" id="comment" name="comment"></textarea>
+                    <?php Messages::printInputErrors( 'comment' ); ?>
+                </div>
+            </div>
+            <div class="form__row">
+                <input class="form__input form__input--submit button button--primary" id="submit" name="create_comment" type="submit" value="<?= _( 'Create Comment' ) ?>">
+            </div>
+        </form>
+    </section>
+
 </main>
