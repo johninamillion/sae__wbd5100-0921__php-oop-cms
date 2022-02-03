3239c239b3aa57e13f669cdae898890f43ba3223
johninamillion <johninamio@gmail.com>
2022-02-03 12:11:57 +0100

    fix action and method on form tag

--- templates/navigation.php
+++ templates/navigation.php
@@ -9,8 +9,8 @@
                 <li class="navigation__list-item"><a class="navigation__anchor" href="/user/profile"><?= _( 'Profile' ) ?></a></li>
                 <li class="navigation__list-item"><a class="navigation__anchor" href="/logout"><?= _( 'Logout' ) ?></a></li>
             </ul>
-            <form class="form form--inline">
-                <div class="form__row" action="/search" method="GET">
+            <form class="form form--inline" action="/search" method="get">
+                <div class="form__row">
                     <input name="keyword" type="text" placeholder="<?= _( 'Search' ) ?>">
                 </div>
             </form>
