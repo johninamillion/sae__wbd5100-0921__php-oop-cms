c86e1bc9d6294db9f77ac1a26d9e48d059b2e2e7
johninamillion <johninamio@gmail.com>
2022-02-10 11:55:08 +0100

    update navigation template

--- templates/navigation.php
+++ templates/navigation.php
@@ -9,12 +9,12 @@
                 <li class="navigation__list-item"><a class="navigation__anchor" href="/user/profile"><?= _( 'Profile' ) ?></a></li>
                 <li class="navigation__list-item"><a class="navigation__anchor" href="/logout"><?= _( 'Logout' ) ?></a></li>
             </ul>
-            <form class="form form--inline" action="/search" method="get">
-                <div class="form__row">
-                    <input name="keyword" type="text" placeholder="<?= _( 'Search' ) ?>">
-                </div>
-            </form>
-        </nav>
 
-    </div>
-</header>
+            <form id="search-form" class="form form--inline" action="/search" method="get">
+                <input class="form__input form__input--text" name="keyword" type="text" placeholder="<?= _( 'Search' ) ?>">
+            </form><!-- /#search-form -->
+
+        </nav><!-- /#masthead__navigation -->
+
+    </div><!-- /#masthead__inner -->
+</header><!-- /#masthead -->
