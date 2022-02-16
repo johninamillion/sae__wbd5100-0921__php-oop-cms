2a55dbd4e91a6d65ec9dc1013b219086e8e7c832
johninamillion <johninamio@gmail.com>
2022-02-16 11:05:42 +0100

    use api to check if an email or username exists

--- templates/register/index.php
+++ templates/register/index.php
@@ -45,5 +45,82 @@ namespace CMS;
             </div>
         </form>
 
+        <script type="text/javascript">
+            ( function() {
+
+                'use strict';
+
+                var apiUrl = 'http://sae.millionvisions.de/api';
+                var emailInput = document.getElementById( 'email' );
+                var usernameInput = document.getElementById( 'username' );
+
+                function changeEmailCallback( response ) {
+                    if ( this.status === 200 ) {
+                        var parsed = JSON.parse( this.responseText );
+
+                        if ( parsed[ 'email_exists' ] === true ) {
+                            emailInput.classList.add( 'form__input--error' );
+                        }
+                        else {
+                            emailInput.classList.remove('form__input--error');
+                        }
+                    }
+                    else {
+                        alert( 'BAD REQUEST!' );
+                    }
+                }
+
+                function changeUsernameCallback( response ) {
+                    if ( this.status === 200 ) {
+                        var parsed = JSON.parse( this.responseText );
+
+                        if ( parsed[ 'username_exists' ] === true ) {
+                            usernameInput.classList.add( 'form__input--error' );
+                        }
+                        else {
+                            usernameInput.classList.remove('form__input--error');
+                        }
+                    }
+                    else {
+                        alert( 'BAD REQUEST!' );
+                    }
+                }
+
+                function changeEmail( event ) {
+                    var target = event.target;
+                    var value = target.value;
+                    var url = apiUrl + '/emailExists?email=' + value;
+
+                    console.log( 'change email' );
+                    console.log( url );
+
+                    var XHR = new XMLHttpRequest();
+                    XHR.callback = changeEmailCallback;
+                    XHR.onload = changeEmailCallback;
+                    XHR.open( 'GET', url );
+                    XHR.send( null );
+                }
+
+                function changeUsername( event ) {
+                    var target = event.target;
+                    var value = target.value;
+                    var url = apiUrl + '/usernameExists?username=' + value;
+
+                    console.log( 'change username' );
+                    console.log( url );
+
+                    var XHR = new XMLHttpRequest();
+                    XHR.callback = changeUsernameCallback;
+                    XHR.onload = changeUsernameCallback;
+                    XHR.open( 'GET', url );
+                    XHR.send( null );
+                }
+
+                usernameInput.addEventListener( 'change', changeUsername );
+                emailInput.addEventListener( 'change', changeEmail );
+
+            } )();
+        </script>
+
     </div>
 </main>
